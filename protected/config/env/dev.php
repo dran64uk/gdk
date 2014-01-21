58e76b7642e70a2830ed8e7c08dc53fd39ae38e9
Serhiy Vinichuk <serhiyvinichuk@gmail.com>
2014-01-21 19:04:34 +0200

    add env config

---
+++ protected/config/env/dev.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Local cofigurations
+ */
+return array(
+		'components'=>array(
+			'db'=>array(
+				'connectionString' => 'mysql:host=localhost;dbname=photosite',
+				'emulatePrepare' => true,
+				'username' => 'root',
+				'password' => '',
+				'charset' => 'utf8',
+			),
+		),
+	);
+ ?>
