f749f00df9d142561acd29ba04ca67f7cafda58b
Serhiy Vinichuk <serhiyvinichuk@gmail.com>
2014-01-21 19:37:10 +0200

    add CRUD

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,20 +1,19 @@
 <?php
 /* @var $this SiteController */
-
+/* @var $model Album with Photos */
 $this->pageTitle=Yii::app()->name;
 ?>
+<h1>Albums</h1>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<?php
+	foreach ($model as $album) {
+		echo "<h2>$album->title</h2>";
+		foreach ($album->photos as $photo) {
+			echo $photo->title . "<br>";
+			echo CHtml::image(Yii::app()->getBaseUrl(true) . $photo->path) . "<br>";
+		}
+		echo "<hr>";
+	}
+	
+		
+?>
