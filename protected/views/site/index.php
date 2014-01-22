358f040c0b7fa3089c684c7f888456aec7d6e095
edkbil <edkbil@mail.ru>
2014-01-22 10:23:41 +0200

    fix images preview size

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -10,7 +10,7 @@ $this->pageTitle=Yii::app()->name;
 		echo "<h2>$album->title</h2>";
 		foreach ($album->photos as $photo) {
 			echo $photo->title . "<br>";
-			echo CHtml::image(Yii::app()->getBaseUrl(true) . $photo->path) . "<br>";
+			echo CHtml::image(Yii::app()->getBaseUrl(true) . $photo->path, '', array('style' => 'width: 250px;')) . "<br>";
 		}
 		echo "<hr>";
 	}
