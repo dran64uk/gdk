babf5e7bda0325808f60295069fd1c8c2d2ab27e
Yura4ina <Win32J@mail.ru>
2014-01-27 09:14:08 +0200

    add album link

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -7,9 +7,9 @@ $this->pageTitle=Yii::app()->name;
 
 <?php
 	foreach ($model as $album) {
-		echo "<h2>$album->title</h2>";
+		echo CHtml::link("<h2>$album->title</h2>", array('album/view/'.$album->id));
 		foreach ($album->photos as $photo) {
-			echo $photo->title . "<br>";
+			echo $photo->title. "<br>";
 			echo CHtml::link( 
 			CHtml::image(Yii::app()->getBaseUrl(true) . $photo->path, '', array('style' => 'width: 250px;')) . "<br>",
 			array('/photo/view/'.$photo->id)
